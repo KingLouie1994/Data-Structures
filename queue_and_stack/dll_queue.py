ef8d6487e89a60e9bb24bfff430fa734306f9af7
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-04-20 16:15:21 +0200

    Queue and stack: Queue class works and passes tests

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -6,14 +6,28 @@ from doubly_linked_list import DoublyLinkedList
 class Queue:
     def __init__(self):
         self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        """
+        Why is our DLL a good choice to store our elements?
+        
+        A Queue is there to add elements to the tail and remove elements from the head. On linked lists 
+        adding and removing elements from head and tail as a complexity of O(1)
+        """
+        
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        if self.size == 0:
+            return None
+        else:
+            self.size -= 1
+            return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        if self.size > 0:
+            return self.size
+        else:
+            return 0
